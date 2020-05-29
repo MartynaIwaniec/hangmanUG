fd1b01b85d41541c6ce272e2381b09de1530a765
MartynaIwaniec <44570285+MartynaIwaniec@users.noreply.github.com>
2020-05-29 15:53:18 +0200

    Update wisielec.py

--- wisielec.py
+++ wisielec.py
@@ -1,28 +1,5 @@
 import random
-import sys
 import time
-
-pl_animals = ['piesek', 'żyrafa', 'tygrys', 'jaskółka', 'hipopotam', 'tygrys', 'szympans', 'krokodyl', 'delfin',
-              'niedźwiedź', 'antylopa', 'gepard', 'pantera']
-pl_fruit = ['jabłko', 'ananas', 'truskawki', 'porzeczki', 'maliny', 'brzoskwinia', 'borówki', 'gruszka', 'cytryna',
-            'jagoda', 'mandarynka', 'pomarańcza', 'winogrona']
-pl_jobs = ['marynarz', 'nauczyciel', 'sportowiec', 'programista', 'kierowca', 'sprzedawca', 'dziennikarz', 'piosenkarz',
-           'architekt', 'piekarz', 'fotograf', 'lekarz']
-
-eng_animals = ['elephant', 'crocodile', 'panther', 'monkey', 'gorilla', 'cheetah', 'chameleon', 'chicken', 'hedgehog',
-               'giraffe', 'butterfly', 'rabbit', 'gosling']
-eng_fruit = ['strawberry', 'raspberry', 'pineapple', 'orange', 'honeydew', 'bananas', 'coconut', 'blueberry',
-             'watermelon', 'dragonfruit', 'passionfruit', 'papaya']
-eng_jobs = ['teacher', 'musician', 'dancer', 'programmer', 'builder', 'firefighter', 'policeman', 'photographer',
-            'waitress', 'baker', 'lawyer', 'journalist', 'astronaut']
-
-ger_animals = ['elefant', 'gorilla', 'gepard', 'krokodil', 'kaninchen', 'schmetterling', 'giraffe', 'schwein', 'frosch',
-               'pferde', 'fisch', 'hund', 'vogel', 'schlange']
-ger_fruit = ['zitrone', 'wasseermelone', 'orange', 'trauben', 'apfel', 'ananas', 'melone', 'birne', 'erdbeere',
-             'blaubeere', 'kirsche', 'limone', 'pflaume']
-ger_jobs = ['bauer', 'anwalt', 'ingenieur', 'lehrerin', 'kellner', 'pilot', 'richter', 'bibliothekar', 'politikerin',
-            'architektin', 'schriftsteller', 'friseur']
-
 
 def main_menu():
     print("\n \t\t ——————————————————————— \n\t\t|    W I S I E L E C    | \n\t\t ———————————————————————\n")
@@ -31,9 +8,9 @@ def main_menu():
     time.sleep(1)
     print("\nCo chcesz zrobić? Wpisz odpowiednią cyfrę!")
     time.sleep(1)
-    print("\n[1] Nowa gra \n[2] Zakończ \n[3] Zobacz ranking wyników \n\n> ")
+    print("\n[1] Nowa gra \n[2] Zakończ \n[3] Zobacz ranking wyników \n> ")
     time.sleep(1)
-
+    play()
 
 def play():
     user_input = input()
@@ -41,56 +18,19 @@ def play():
     time.sleep(1)
     if user_input == "1":
         hangman()
-
     elif user_input == "2":
         print("Dzięki za grę, miłego dnia!\n")
         quit()
     elif user_input == "3":
-        print("Jeśli jeszcze nie grałeś, wpisz 0!\n")
-        top_scorers()
-        quit()
+        try:
+            print_winners()
+        except FileNotFoundError:
+            print("\nNie ma jeszcze żadnych wyników na liście zwycięzców!\nZagraj w grę, by dostać się na listę!")
+            time.sleep(1)
+            yes_no()
     else:
-        print("Błąd! Niepoprawny wybór!")
-        quit()
-
-
-#
-# def get_high_score():
-#     high_score = 0
-#     try:
-#         high_score_file = open("high_score.txt", "r")
-#         high_score = int(high_score_file.read())
-#         high_score_file.close()
-#         print("Najwyższy wynik to ", high_score)
-#     except IOError:
-#         print("Nie ma jeszcze żadnych wyników.")
-#     except ValueError:
-#         print("Wprowadziłeś niepoprawny wynik :(")
-#     return high_score
-#
-#
-# def save_high_score(new_high_score):
-#     try:
-#         high_score_file = open("high_score.txt", "w")
-#         high_score_file.write(str(new_high_score))
-#         high_score_file.close()
-#     except IOError:
-#         print("Błąd! Nie można zapisać wyniku!")
-#
-#
-# def rank():
-#     high_score = get_high_score()
-#     current_score = 0
-#     try:
-#          current_score = int(input("Jaki jest Twój wynik?"))
-#     except ValueError:
-#         print("Błąd! Wynik musi być liczbą!")
-#
-#     if current_score > high_score:
-#         print("Brawo! Nowy rekord!")
-#         save_high_score(current_score)
-#     else:
-#         print("Niestety nie udało Ci się pobić rekordu. Powodzenia następnym razem :)")
+        print("\nBłąd! Niepoprawny wybór!")
+        main_menu()
 
 
 def graphics(guesses, answer):
@@ -99,6 +39,7 @@ def graphics(guesses, answer):
 
     elif (guesses == 1):
         print("""
+
 
 
 
@@ -126,7 +67,7 @@ def graphics(guesses, answer):
         print("""
                            ____
                           |    |
-                          |    o
+                          |    O
                           |  
                           |  
                           |   
@@ -140,7 +81,7 @@ def graphics(guesses, answer):
         print("""
                            ____
                           |    |
-                          |    o
+                          |    O
                           |    | 
                           |    |
                           |    
@@ -157,7 +98,7 @@ def graphics(guesses, answer):
         print("""
                            ____
                           |    |
-                          |    o
+                          |    O
                           |  / | 
                           |    |
                           |   
@@ -171,7 +112,7 @@ def graphics(guesses, answer):
         print("""
                            _____
                           |     |
-                          |     o
+                          |     O
                           |   / | \\
                           |     |
                           |   
@@ -188,7 +129,7 @@ def graphics(guesses, answer):
         print("""
                            _____
                           |     |
-                          |     o
+                          |     O
                           |   / | \\
                           |     |
                           |    /
@@ -203,7 +144,7 @@ def graphics(guesses, answer):
         print("""
                                _____
                               |     |
-                              |     o
+                              |     O
                               |   / | \\
                               |     |
                               |    / \\
@@ -214,23 +155,44 @@ def graphics(guesses, answer):
             \n
                             """)
         time.sleep(1)
-        print("Hasło brzmiało: " + answer.upper() + "\n")
+        print("\nHasło brzmiało: " + answer.upper() + "\n")
         time.sleep(1)
-        print("\nPrzegrałes :( \nCzy chcesz zagrać jeszcze raz? Wprowadź odpowiednią cyfrę.")
-        time.sleep(1)
-        print("\n[1] Tak \n[2] Nie")
-        time.sleep(1)
+        print("\nPrzegrałeś :( \n")
         yes_no()
         return
 
 
+pl_animals = ['piesek', 'żyrafa', 'tygrys', 'jaskółka', 'hipopotam', 'tygrys', 'szympans', 'krokodyl', 'delfin',
+              'niedźwiedź', 'antylopa', 'gepard', 'pantera']
+pl_fruit = ['jabłko', 'ananas', 'truskawki', 'porzeczki', 'maliny', 'brzoskwinia', 'borówki', 'gruszka', 'cytryna',
+            'jagoda', 'mandarynka', 'pomarańcza', 'winogrona']
+pl_jobs = ['marynarz', 'nauczyciel', 'sportowiec', 'programista', 'kierowca', 'sprzedawca', 'dziennikarz', 'piosenkarz',
+           'architekt', 'piekarz', 'fotograf', 'lekarz']
+
+eng_animals = ['elephant', 'crocodile', 'panther', 'monkey', 'gorilla', 'cheetah', 'chameleon', 'chicken', 'hedgehog',
+               'giraffe', 'butterfly', 'rabbit', 'gosling']
+eng_fruit = ['strawberry', 'raspberry', 'pineapple', 'orange', 'honeydew', 'bananas', 'coconut', 'blueberry',
+             'watermelon', 'dragonfruit', 'passionfruit', 'papaya']
+eng_jobs = ['teacher', 'musician', 'dancer', 'programmer', 'builder', 'firefighter', 'policeman', 'photographer',
+            'waitress', 'baker', 'lawyer', 'journalist', 'astronaut']
+
+ger_animals = ['elefant', 'gorilla', 'gepard', 'krokodil', 'kaninchen', 'schmetterling', 'giraffe', 'schwein', 'frosch',
+               'pferde', 'fisch', 'hund', 'vogel', 'schlange']
+ger_fruit = ['zitrone', 'wasseermelone', 'orange', 'trauben', 'apfel', 'ananas', 'melone', 'birne', 'erdbeere',
+             'blaubeere', 'kirsche', 'limone', 'pflaume']
+ger_jobs = ['bauer', 'anwalt', 'ingenieur', 'lehrerin', 'kellner', 'pilot', 'richter', 'bibliothekar', 'politikerin',
+            'architektin', 'schriftsteller', 'friseur']
+
+
 def selectWord():
-    print("Czy chcesz wgrać plik .txt czy użyć jednego z domyślnych słów? Wprowadź odpowiednią cyfrę.")
+    print("\nCzy chcesz wgrać plik .txt czy użyć jednego z domyślnych słów? Wprowadź odpowiednią cyfrę.\n")
     time.sleep(1)
     user_choice = input("[1] Mój plik .txt \n[2] Losowe gotowe hasło \n> ")
-    if user_choice == "1":
-        # f = input("Proszę wpisać ścieżkę do pliku, z którego pochodzić będzie hasło.")
-        f = 'C:\\Users\marty\OneDrive\Pulpit\positive-words.txt'
+    if user_choice == "wstecz":
+        main_menu()
+    elif user_choice == "1":
+        f = "C:\\Users\marty\OneDrive\Pulpit\positive-words.txt"
+        #f = input("\nProszę wpisać pełną ścieżkę do pliku, z którego pochodzić będzie hasło.\n> ")
         file = open(f, 'r')
         words = file.readlines()
         my_word = ''
@@ -249,12 +211,22 @@ def selectWord():
         return my_word
 
     elif user_choice == "2":
-        language = input(
-            "Jaki język wybierasz? Wprowadź odpowiednią cyfrę.\n [1] Polski \n [2] Angielski \n [3] Niemiecki \n> ")
-        if language == "1":
-            category = input(
-                "Jaką kategorię wybierasz? Wprowadź odpowiednią cyfrę.\n [1] zwierzątka \n [2] owoce \n [3] zawody \n")
-            if category == "1":
+        time.sleep(1)
+        print("\nJaki język wybierasz?\n")
+        time.sleep(1)
+        language = input("Wprowadź odpowiednią cyfrę.\n [1] Polski \n [2] Angielski \n [3] Niemiecki \n> ")
+
+        if language == "wstecz":
+            selectWord()
+        elif language == "1":
+            print("\nUwaga! Pamiętaj, że w przygotowanych hasłach występują polskie znaki!\n")
+            time.sleep(1)
+            print("Jaką kategorię wybierasz?\n")
+            time.sleep(1)
+            category = input("Wprowadź odpowiednią cyfrę.\n [1] zwierzątka \n [2] owoce \n [3] zawody\n> ")
+            if category == "wstecz":
+                selectWord()
+            elif category == "1":
                 word = random.choice(pl_animals)
                 word = word.lower()
             elif category == "2":
@@ -265,10 +237,15 @@ def selectWord():
                 word = word.lower()
             return word
 
+        elif language == "wstecz":
+            selectWord()
         elif language == "2":
-            category = input(
-                "Jaką kategorię wybierasz? Wprowadź odpowiednią cyfrę.\n [1] animals \n [2] fruit \n [3] jobs \n")
-            if category == "1":
+            print("\nJaką kategorię wybierasz?\n")
+            time.sleep(1)
+            category = input("Wprowadź odpowiednią cyfrę.\n [1] animals \n [2] fruit \n [3] jobs\n> ")
+            if category == "wstecz":
+                selectWord()
+            elif category == "1":
                 word = random.choice(eng_animals)
                 word = word.lower()
             elif category == "2":
@@ -279,10 +256,15 @@ def selectWord():
                 word = word.lower()
             return word
 
+        elif language == "wstecz":
+            selectWord()
         elif language == "3":
-            category = input(
-                "Jaką kategorię wybierasz? Wprowadź odpowiednią cyfrę.\n [1] Tiere\n [2] Obst\n [3] Beruf\n")
-            if category == "1":
+            print("\nJaką kategorię wybierasz?\n")
+            time.sleep(1)
+            category = input("Wprowadź odpowiednią cyfrę.\n [1] Tiere\n [2] Obst\n [3] Beruf\n> ")
+            if category == "wstecz":
+                selectWord()
+            elif category == "1":
                 word = random.choice(ger_animals)
                 word = word.lower()
             elif category == "2":
@@ -292,10 +274,12 @@ def selectWord():
                 word = random.choice(ger_jobs)
                 word = word.lower()
             return word
+        else:
+            selectWord()
 
     else:
         time.sleep(1)
-        print("Błąd! Wprowadź swój wybór ponownie!")
+        print("Błąd! Wprowadź swój wybór ponownie!\n")
         selectWord()
 
 
@@ -313,53 +297,40 @@ def hangman():
     graphics(guesses, word)
     print("\n" + "" + ' '.join(blanks_list) + "\n")
     time.sleep(1)
-    print("Wpisz literę!")
+    print("Wpisz literę!\n")
 
     while guesses < 8:
-        guess = str(input("> "))
+        guess = str(input("\n> "))
         guess = guess.lower()
-        score = 8
         if guess == "pomoc":
             if word[0] not in guess_list:
-                print("Pierwsza litera to: " + word[0])
-                score = 7
+                print("\n\t\tPierwsza litera to: " + word[0])
             elif word[1] not in guess_list:
-                print("Druga litera to: " + word[1])
-                score = 6.5
+                print("\n\t\tDruga litera to: " + word[1])
             elif word[2] not in guess_list:
-                print("Trzecia litera to: " + word[2])
-                score = 6
+                print("\n\t\tTrzecia litera to: " + word[2])
             elif word[3] not in guess_list:
-                print("Czwarta litera to: " + word[3])
-                score = 5.5
+                print("\n\t\tCzwarta litera to: " + word[3])
             elif word[4] not in guess_list:
-                print("Piąta litera to: " + word[4])
-                score = 5
+                print("\n\t\tPiąta litera to: " + word[4])
             elif word[5] not in guess_list:
-                print("Szósta litera to: " + word[5])
-                score = 4
+                print("\n\t\tSzósta litera to: " + word[5])
             elif word[6] not in guess_list:
-                print("Siódma litera to: " + word[6])
-                score = 2
+                print("\n\t\tSiódma litera to: " + word[6])
             elif word[3] not in guess_list:
-                print("Ósma litera to: " + word[7])
-                score = 2
+                print("\n\t\tÓsma litera to: " + word[7])
             elif word[4] not in guess_list:
-                print("Dziewiąta litera to: " + word[8])
-                score = 1
+                print("\n\t\tDziewiąta litera to: " + word[8])
             elif word[-2] not in guess_list:
-                print("Przedostatnia litera to: " + word[-2])
-                score = 0
+                print("\n\t\tPrzedostatnia litera to: " + word[-2])
             elif word[-1] not in guess_list:
-                print("Ostatnia litera to: " + word[-1])
-                score = 0
-            final_score = score
+                print("\n\t\tOstatnia litera to: " + word[-1])
         elif len(guess) > 1:
-            print("Wpisz tylko jedną literę.")
+            print("\t\tWpisz tylko jedną literę!\n")
         elif guess == "":
-            print("Wpisz literę!")
+            print("\t\tWpisz literę!")
         elif guess in guess_list:
-            print("Już użyłeś tej litery! Wpisz nową!")
+            print("\t\tJuż użyłeś tej litery! Wpisz nową!\n")
             print(' '.join(guess_list))
         else:
             guess_list.append(guess)
@@ -369,11 +340,11 @@ def hangman():
                     new_blanks_list[i] = word_list[i]
                 i = i + 1
             if new_blanks_list == blanks_list:
-                print("Tej litery nie ma w haśle.")
+                print("\t\tTej litery nie ma w haśle.\n")
                 guesses = guesses + 1
                 graphics(guesses, word)
                 if guesses < 8:
-                    print("Zgaduj dalej!")
+                    print("\t\tZgaduj dalej!\n")
                     print(' '.join(blanks_list))
 
             elif word_list != blanks_list:
@@ -381,53 +352,53 @@ def hangman():
                 print(' '.join(blanks_list))
 
                 if word_list == blanks_list:
-                    print("\nBrawo! Wygrałeś!")
-                    print("Twój wynik to: ", final_score)
-                    top_scorers()
+                    print("\n\t\tBrawo! Wygrałeś!")
+                    time.sleep(1)
+                    name = str(input("\nPodaj swoje imię: "))
+                    get_winner(name)
                     print("\n")
                     time.sleep(1)
-                    # print("Czy chcesz zagrać jeszcze raz? Wprowadź odpowiednią cyfrę.")
-                    # time.sleep(1)
-                    # print("[1] Tak \n[2] Nie")
-                    yes_no()
                 else:
-                    print("Dobry strzał! Zgaduj dalej!")
+                    x = ["Dobry strzał! Zgaduj dalej!", "Oby tak dalej!", "Wyśmienicie :)", "Super! Jesteś już blisko zwycięstwa!"]
+                    print("\n\t\t", random.choice(x))
 
 
-def top_scorers():
-    final_score = hangman()
-    user = str(input("Podaj swoje imię: "))
-    file = open("top_scores.txt", "a")
+def get_winner(name):
+    file = open("top_winners.txt", "a")
     file.write("\n")
-    file.write(user)
-    file.write(",")
-    file.write(str(final_score))  # (int(x)) can not be written
-    file.write("punktów")
+    file.write(name)
     file.write("\n")
     file.close()
+    print_winners()
 
+
+def print_winners():
     print("\n")
-    print("⬇ LISTA WYNIKÓW ⬇")
-    f = open('top_scores.txt', 'r')
+    print("⬇ LISTA ZWYCIĘZCÓW ⬇")
+    f = open('top_winners.txt', 'r')
     winners = [line.replace('\n', '') for line in f.readlines()]
     for i in winners:
         print(i)
     f.close()
-    time.sleep(5)
-    sys.exit()
+    time.sleep(2)
+    yes_no()
 
 
 def yes_no():
-    again = str(input("> "))
+    print("\nCzy chcesz zagrać nową rozgrywkę? Wybierz odpowiednią cyfrę.")
+    time.sleep(1)
+    print("\n[1] Tak \n[2] Nie \n> ")
+    time.sleep(1)
+    again = str(input())
     if again == "1":
         hangman()
     elif again == "2":
+        time.sleep(1)
         print("Dzięki za grę! Miłego dnia!")
         time.sleep(1)
-        top_scorers()
-        # score
         quit()
     else:
+        time.sleep(1)
         print("Wprowadź poprawną cyfrę.")
         time.sleep(1)
         yes_no()
@@ -436,16 +407,5 @@ def yes_no():
 def main():
     main_menu()
     time.sleep(1)
-    play()
-    # if input("\nKliknij ENTER, aby kontynuować lub wpisz 'koniec' by wyjść z gry.") == "koniec":
-    #     quit()
-    # else:
-    #     time.sleep(1)
-    #     hangman()
-    time.sleep(1)
     hangman()
-
-
 main()
-
-# Martyna Iwaniec I MA PJN
